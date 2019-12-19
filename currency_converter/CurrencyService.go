114daefcc193a268385115e394eac5703cf90aa7
dan <me@danrh.co.uk>
2019-12-19 09:34:52 +0000

    Add index to currency converter

--- currency_converter/CurrencyService.go
+++ currency_converter/CurrencyService.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -45,14 +46,20 @@ func getConversionRate(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Available methods:\n/rates \n/codes\n/rate/{currency1}/{currency2} e.g. /rate/AER/USD")
+}
+
 func main() {
 	ratesFile := flag.String("file", "exchange_rates.csv", "The path of the exchange rates csv file")
 	port := flag.String("port", "8050", "the port on which to serve the server")
-        flag.Parse()
-        rates = loadRates(*ratesFile)
+	flag.Parse()
+	rates = loadRates(*ratesFile)
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/rates", getRates).Methods("GET")
 	router.HandleFunc("/codes", getCurrencyCodes).Methods("GET")
 	router.HandleFunc("/rate/{c1}/{c2}", getConversionRate).Methods("GET")
+	router.HandleFunc("/", getIndex).Methods("GET")
 	log.Fatal(http.ListenAndServe(":"+*port, router))
 }
