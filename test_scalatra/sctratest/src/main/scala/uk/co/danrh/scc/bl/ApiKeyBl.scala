5760a357e42bd0f67a88beca1e368643dcf207f2
dan <me@danrh.co.uk>
2020-01-14 16:58:00 +0000

    ApiKeyBl has method getUserId to get the UserId for an ApiKey

--- test_scalatra/sctratest/src/main/scala/uk/co/danrh/scc/bl/ApiKeyBl.scala
+++ test_scalatra/sctratest/src/main/scala/uk/co/danrh/scc/bl/ApiKeyBl.scala
@@ -10,18 +10,20 @@ trait ApiKeyBl {
 
 
   def md5(s: String) = new String(new BigInteger(1,MessageDigest.getInstance("MD5").digest(s.getBytes)).toString())
-
-  private var session_keys = List[String]()//todo use more appropriate data structure
+  case class SessionKey(key: String, userId: String)
+  private var sessionKeys = List[SessionKey]()//todo use more appropriate data structure
 
   def createKey(id:String,hash:String):Either[ResponseCode.Failed, ApiKey] = {
     if (UsersBl.getUser(id).pwhash==hash) {
-      val key = md5(id + hash)
-      session_keys = key :: session_keys
-      Right(ApiKey(key))
+      val key = SessionKey( md5(id + hash), id)
+      sessionKeys = key :: sessionKeys
+      Right(ApiKey(key.key))
     } else Left(ResponseCode.Failed())
   }
 
-  def checkKeyValid(key: ApiKey):Boolean = session_keys.contains(key.key)
+  def checkKeyValid(key: ApiKey):Boolean = sessionKeys.exists(_.key == key.key)
+
+  def getUserId(key: String): String = sessionKeys.filter(_.key == key).head.userId
 }
 
 object ApiKeyBl extends ApiKeyBl
